9c12c2461da34d13325d4b50a3fbc7eee48ae081
Andrew Kane <andrew@ankane.org>
2022-07-31 21:46:04 -0700

    Updated pgvector to 0.2.7

--- Formula/pgvector.rb
+++ Formula/pgvector.rb
@@ -1,8 +1,8 @@
 class Pgvector < Formula
   desc "Open-source vector similarity search for Postgres"
   homepage "https://github.com/pgvector/pgvector"
-  url "https://github.com/pgvector/pgvector/archive/v0.2.6.tar.gz"
-  sha256 "8ec8e3a7558e6e88e468671ef532a88a9e27e3245ceb6c7720426d67e485adf1"
+  url "https://github.com/pgvector/pgvector/archive/v0.2.7.tar.gz"
+  sha256 "d22439cb40b9cb28323aa38521a2fc807f08a86daf05d36f08bcf45b5f1b7221"
   license "PostgreSQL"
 
   depends_on "postgresql"
